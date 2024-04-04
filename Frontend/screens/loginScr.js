a2d1b3a0af1cfcd2b8640597141b1ea6613a682c
Luke Fisher <lukeantoniofisher@gmail.com>
2024-04-04 14:11:43 -0400

    Backend and Frontend Setup

---
+++ Frontend/screens/loginScr.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default class LoginScr extends React.Component {
+    render() {
+        return (
+                <text>test</text>
+        )
+    }
+}
---
+++ Frontend/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+let test = require("./index.js");
+export default class App extends React.Component {
+    render() {
+        return (
+            test
+        )
+    }
+}
