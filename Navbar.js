bdd1abc46411780625d09caaae1740c69bdecfe0
Cadew87 <128644981+Cadew87@users.noreply.github.com>
2024-04-24 16:26:01 -0400

    Navbar with working login and logout scenarios
    This should be ready for final implementation.

--- Navbar.js
+++ Navbar.js
@@ -1,38 +1,105 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import React, { useContext } from 'react';
 import UserContext from './UserContext';
-import LoginScr from "./pages/loginScr";
-//import Logout from "./pages/Logout";
-import LogoutCopy from "./LogoutCopy";
-
 import { useState, useEffect } from 'react';
-import Logout from "./pages/Logout";
-
-
 
 export default function Navbar() {
-    const { userImage } = useContext(UserContext);
-    const [user, setuser] = useState("Login")
     const [ch, setch] = useState(false)
+    const username = ["test", "Cade", "Luke", "Michael"]
+    const password = ["password", "password", "password", "password"]
+    const [user, setuser] = useState("")
+    const [pass, setpass] = useState("")
+    const [status, setstatus] = useState("")
+    const { userImage } = useContext(UserContext);
+    const [usera, setusera] = useState("")
+    if (!ch){
 
-    if (!ch) {
-        console.log(Navbar)
-        if (LoginScr.toString().length < 25){
-            setch(true)
+
+        const check_log = () => {
+            let t = false
+            let pp = 0
+            for (let i = 0; i < username.length; i++) {
+                if (username[i] == user){
+                    if (password[i] == pass){
+                        t = !t
+                        pp = i
+                        break
+                    }
+                }
+            }
+            if (t){
+                setuser("")
+                setpass("")
+                setstatus("")
+                setusera(user)
+                setch(true)
+            }else{
+                setstatus("Incorrect Username/Password")
+            }
+        }
+
+        const check_reg = () => {
+            let t = true
+            for (let i = 0; i < username.length; i++) {
+                if (username[i] == user){
+                    t = !t
+                    break
+                }
+            }
+            if (t){
+
+                setstatus("Successful Registration")
+                console.log("Username: ", user)
+                console.log("Password: ", pass)
+                username.push(user)
+                password.push(pass)
+            }else{
+                setstatus("Unsuccessful Registration")
+            }
+
+
+
         }
         return (
-            <nav className="nav">
-                <Link to="/" className="site-title">
-                    <img src="./img/st-logo.svg" alt="Steam Tracker Logo"/>
-                </Link>
-                <ul>
-                    <CustomLink to="/Login">Login</CustomLink>
-                </ul>
-            </nav>
+
+            <div className="Title">
+
+
+                <div className="App">
+
+                    <div className="Login/Register">
+                        <h1>Login/Register
+                        </h1>
+                        <div className="User">
+                            <input type="text" placeholder="Username"
+                                   onChange={(e) => setuser(e.target.value)}/>
+                        </div>
+                        <div className="Pass">
+                            <input type="text" placeholder="Password"
+                                   onChange={(e) => setpass(e.target.value)}/>
+                        </div>
+                        <div className="Status">
+                            <label>{status}</label>
+                        </div>
+                        <div className="LB">
+                            <button onClick={check_log}>Login</button>
+                        </div>
+                        <div className="RB">
+                            <button onClick={check_reg}>Register</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
-    if (ch) {
 
+
+
+
+    if(ch) {
+        const check = () => {
+            setch(false)
+        }
 
         return (
             <nav className="nav">
@@ -47,15 +114,22 @@ export default function Navbar() {
                     <CustomLink to="/User">
                         <img className="user-img" src={userImage} alt="User"/>
                     </CustomLink>
-                    <CustomLink to="/Logout">Logout</CustomLink>
+                    <div className="Butt">
+                        <text>Hi {usera}</text>
+                        <div className="Button">
+                            <button onClick={check}>Logout</button>
+                        </div>
+                    </div>
                 </ul>
+
+
             </nav>
-        );
+    );
     }
 
-}
+    }
 
-function CustomLink({ to, children, ...props }) {
+    function CustomLink({to, children, ...props}) {
     const resolvedPath = useResolvedPath(to);
     const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
