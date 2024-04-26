5d53bc8ab400bc2e671675c2e5b83157503eb6e5
Cadew87 <128644981+Cadew87@users.noreply.github.com>
2024-04-26 00:15:10 -0400

    Add files via upload

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,1 +1,27 @@
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const Usermod = require('./models/User')
 
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://cadewcocoabeach:root@cluster0.lyblfks.mongodb.net/Final?retryWrites=true&w=majority")
+
+app.get("/getUsers", (req, res) => {
+    Usermod.find().then(users => res.json(users)).catch(err => res.json(err));
+});
+
+app.post("/addUser", async (req, res) => {
+    const user = req.body
+    const newuser = new Usermod(user)
+    await newuser.save()
+
+    //res.json(user)
+})
+
+app.listen(3001, () => {
+    console.log("Server Runs")
+})
