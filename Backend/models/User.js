e4f09bcfb544c19ecf22fd1ce329bee8f69492bc
Cadew87 <128644981+Cadew87@users.noreply.github.com>
2024-04-26 00:15:49 -0400

    Add files via upload

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose')
 
+const UserSch = new mongoose.Schema({
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    }
+)
+
+const Usermod = mongoose.model("users", UserSch)
+module.exports = Usermod
